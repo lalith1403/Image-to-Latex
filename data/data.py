aeeb7f1e269c8fd241aed48e9d626537850c93bc
Lalithnarayan C <26842636+lalith1403@users.noreply.github.com>
2021-05-15 20:56:36 +0530

    add all files

--- data/data.py
+++ data/data.py
@@ -1,14 +1,14 @@
 import tarfile
 
-#simple function to extract the train data
-#tar_file : the path to the .tar file
-#path : the path where it will be extracted
+# simple function to extract the train data
+# tar_file : the path to the .tar file
+# path : the path where it will be extracted
 def extract(tar_file, path):
     opened_tar = tarfile.open(tar_file)
      
     if tarfile.is_tarfile(tar_file):
         opened_tar.extractall(path)
     else:
-        print("The tar file you entered is not a tar file")
+        print("The tar file entered is not a tar file")
 
 extract('formula_images.tar.gz','D:\FSDL\im2latex\data')
